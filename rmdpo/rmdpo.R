1d6697b36a0bafbb727a16b3da895f9078395f71
Philippe Grosjean <phgrosjean@sciviews.org>
2024-08-01 13:45:09 +0200

    TODO list for rmdpo.R

--- rmdpo/rmdpo.R
+++ rmdpo/rmdpo.R
@@ -4,6 +4,18 @@
 #                     Christian Wiat <w9204-r@yahoo.com>
 # (probably to be released as the {rmdpo} R package under MIT license)
 
+# TODO:
+# - Refactor code (avoid duplicates) and place in an R package
+# - Add an example vignette underlining various key points
+# - Allow for a different directory (e.g., data.table -> data.table.fr)
+# - Check on Windows, macOS & Linux, especially
+#   https://github.com/mondeja/md-ulb-pwrap/issues/7
+# - Compare current approach versus pot -> po mechanism
+# - More metadata, particularly, version from DESCRIPTION + branch + commit
+# - Discuss and advertise here:
+#   https://github.com/Rdatatable/data.table/issues/6221 and here:
+#   https://github.com/RConsortium/multilingual-documentation-wg
+
 #' Create a poEdit file from an R Markdown or Quarto document
 #'
 #' @param rmdfile The path to the R Markdown or Quarto document to translate
